3ba0cf86254f3753050ab92ba54ae518a48b79c3
DyariMoSharif <dyari.ameen@gmail.com>
2022-10-23 14:33:12 +0300

    Grammar mistake: Survey CTO Done, too

--- main.py
+++ main.py
@@ -25,19 +25,19 @@ if datetime.date(arrow.get(t).date().year,arrow.get(t).date().month,arrow.get(t)
     sys.exit()
 
 print("---------------------------------------")
-print("ONA Data Monitoring ")
+print("Survey CTO Data Monitoring ")
 print("---------------------------------------")
 
 def Emailing():    
     print("---------  Monitoring a minute-by-minute watch to ensure Survey CTO is functioning properly  ---------")    
     try:
-        urllib.request.urlopen("https://www.surveycto.com").getcode()
+        urllib.request.urlopen("https://www.surveycto.cm").getcode()
     except:
         email_sender = 'second.dyari@gmail.com'
         email_password = 'mpuuajuhmwiwbwzs'
         email_receiver = 'dsharif@mercycorps.org'
         subject = 'HIGH ALERT - Survey CTO IS DOWN (Or something went wrong)'
-        body = f" ------------------ The ONA Data platform is down ------------------\n \n We cheched the statue of surveycto.com website at {time.localtime().tm_hour}:{time.localtime().tm_min}:{time.localtime().tm_sec} on {time.localtime().tm_mday}/{time.localtime().tm_mon}/{time.localtime().tm_year} (According to local time) \n\n\n ' -- This is an automatic email fired by a python app. For more information, email Dyari at dyari.ameen@gmail.com '\n\n\n"
+        body = f" ------------------ The Survey CTO platform is down ------------------\n \n We checked the statue of surveycto.com website at {time.localtime().tm_hour}:{time.localtime().tm_min}:{time.localtime().tm_sec} on {time.localtime().tm_mday}/{time.localtime().tm_mon}/{time.localtime().tm_year} (According to local time) \n\n\n ' -- This is an automatic email fired by a python app. For more information, email Dyari at dyari.ameen@gmail.com '\n\n\n"
         em = EmailMessage()
         
         em['From'] = email_sender
